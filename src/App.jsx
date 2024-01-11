937f272b72e5f43f612ce98c68855d4a272b0785
minhaz1010 <minhazrahman1010@gmail.com>
2024-01-11 18:19:38 +0600

    done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Todo from "./components/Todo/Todo"
+
+
+function App() {
+ 
+  return (
+    <>
+    <Todo></Todo>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+
+const Todo = () => {
+  const [sentence, setSentence] = useState("");
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("list"));
+    if (data) {
+      setList(data);
+    }
+  }, []);
+
+  const handleAdd = () => {
+    const updatedList = [...list, sentence];
+    setList(updatedList);
+    localStorage.setItem("list", JSON.stringify(updatedList));
+    setSentence("");
+  };
+
+  const handleDelete = (index) => {
+    const updatedList = list.filter((_, idx) => idx !== index);
+    setList(updatedList);
+    localStorage.setItem("list", JSON.stringify(updatedList));
+  };
+
+  const renderList = () => {
+    if (list.length > 0) {
+      return list.map((item, index) => (
+        <div
+          key={index}
+          className="flex justify-between items-center bg-gray-200 rounded-md p-3 my-2"
+        >
+          <p className="text-lg">{item}</p>
+          <button
+            className="text-sm bg-red-500 px-3 py-1 text-white rounded-md"
+            onClick={() => handleDelete(index)}
+          >
+            Delete
+          </button>
+        </div>
+      ));
+    } else {
+      return <p className="text-center text-gray-500">No items in the list.</p>;
+    }
+  };
+
+  return (
+    <div className="max-w-md mx-auto my-8 p-4 bg-white shadow-2xl  rounded-xl ">
+      <h1 className="text-3xl mb-4 text-center text-indigo-600">Todo List</h1>
+      <div className="flex mb-4">
+        <input
+          type="text"
+          value={sentence}
+          onChange={(e) => setSentence(e.target.value)}
+          className="w-full rounded-md py-2 px-3 border border-gray-300 focus:outline-none"
+          placeholder="Add a new task"
+        />
+        <button
+          onClick={handleAdd}
+          className="ml-2 px-4 py-2 rounded-md bg-indigo-600 text-white"
+        >
+          Add
+        </button>
+      </div>
+      <div className="mt-4">{renderList()}</div>
+    </div>
+  );
+};
+
+export default Todo;
